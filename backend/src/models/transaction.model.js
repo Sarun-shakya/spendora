4b5b954d064df72476e76719807003d8680761f1
Sarun-shakya <sarunshakya414@gmail.com>
2026-06-14 11:00:25 +0545

    transaction model completed

---
+++ backend/src/models/transaction.model.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const transactionSchema = new mongoose.Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    book: {
+        type: Schema.Types.ObjectId,
+        ref: "Book",
+        required: true
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        enum: ["online", "check", "cash"],
+        required: true
+    },
+    transactionType: {
+        type: String,
+        enum: ["cashIn", "cashOut"],
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+
+    amount: {
+        type: Number,
+        required: true
+    },
+    remarks: {
+        type: String
+    },
+    bill: {
+        url: {
+            type: String
+        },
+        public_id: {
+            type: String,
+        }
+    }
+}, { timestamps: true });
+
+export const Transaction = mongoose.model("Transaction", transactionSchema);
