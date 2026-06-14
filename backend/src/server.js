3003a5353d0c3aec05371a505cc7376093471bd0
Sarun-shakya <sarunshakya414@gmail.com>
2026-06-14 12:14:28 +0545

    added user apis

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,35 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+
+// routes
+import userRoutes from './routes/user.route.js';
 
 const app = express();
 
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const port = process.env.PORT || 3000;
+
+// API routes
+app.use('/api/v1/users', userRoutes);
+
 app.get("/", (req, res) => {
-    res.send("API is working");
+    res.send("Spendora API is working");
 })
 
-app.listen(3000, () => {
-    console.log("Server is running");
-})
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB", err);
+    });
